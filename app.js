b9dc96f866189ab1443ceceea170b5aebf85df20
Ulises Dombroski <84770852+Ulises1122@users.noreply.github.com>
2021-06-30 17:06:49 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let audio = document.getElementById('audio');
+
+function autoRepro(){
+    audio.play();
+}
+
+let msg1 = '';
+
+function changeHead1(){
+    msg1 =  document.getElementById('head1').innerHTML = "Te amo. Mucho más de lo que pensás [ahora doble click ;)]";
+}
+
+let msg2 = '';
+
+function changeHead2(){
+    msg2 = document.getElementById('head1').innerHTML = "Infinitamente, y para toda la vida. Gracias por entrar a mi mundo y permitirme ver toda tu magia. Feliz Mesiversario. Atte: Tu novio - Ulises";
+}
